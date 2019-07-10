72e405cb1d83d5d638ead3f1890cdfe096a3c5fb
Souta Kawahara <s.kawahara.cuebic@gmail.com>
2019-07-11 00:05:24 +0900

    first commit

---
+++ src/main/kotlin/white_junk/WhiteJunkApplication.kt
@@ -0,0 +1,11 @@
+package white_junk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhiteJunkApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhiteJunkApplication>(*args)
+}
---
+++ src/test/kotlin/white_junk/WhiteJunkApplicationTests.kt
@@ -0,0 +1,16 @@
+package white_junk
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WhiteJunkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
